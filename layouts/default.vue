945df508be2fafbde7f4d977b5be088dd4e9e208
jisu7487 <jisu2043@gmail.com>
2021-05-26 12:00:13 +0900

    default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <nav calss = "breadcrumb"
-    <ul>
-      <li><nuxt-link to ="/" ><i class="fas fa-home"> 홈으로 </li>
-   </ul>
-</nav>
+    <nav class="breadcrumb">
+      <ul>
+        <li><nuxt-link to="/"><i class="fas fa-home"></i> 홈으로</nuxt-link></li>
+      </ul>
+    </nav>
     <nuxt/>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <i class="fas fa-copyright"></i> 2020-2021, 모든 저작권은 <strong>나지수</strong>에게 있습니다. 
+      </div>
+    </footer>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,44 @@
     <section class="hero is-primary">
       <div class="hero-body">
         <p class="title">
-          음식칼로리 정보
+          <i class="fas fa-utensils"></i>음식칼로리 정보
         </p>
         <p class="subtitle">
           한식종류와 칼로리 정보 2
         </p>
+      </div>
+    </section>
+    <section class="columns">
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>비빔밥 정보</p>
+          </div>
+          <div class="message-body">
+            <figure class="image is-128x128">
+              <img
+                src=https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/220px-Dolsot-bibimbap.jpg
+                alt=""
+              />
+            </figure>
+            밥에 각종 나물과 각종 장[2], 그리고 기타 동물성 재료(계란, 고기, 통조림 참치 등)을 넣고 비벼서 만드는 요리다. 장으로 고추장을 가장 많이 사용하지만, 간장을 소스로 사용하는 경우도 있고, 그 쪽이 원래 요리법인 것으로 알려져 있다. 현대에 와서는 매운 것을 잘 먹지 못하는 외국인에게 비빔밥을 만들어 줄 때 간장을 소스로 이용하곤 하는데, 후술할 마이클 잭슨 비빔밥이 한때 유행했으며, 가장 최근에 미디어에 등장한 경우로는 윤식당2에서 간장 비빔밥이 주 메뉴로 등장한 바 있다
+          </div>
+        </article>
+      </div>
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>불고기 정보</p>
+          </div>
+          <div class="message-body">
+              <figure class="image is-128x128">
+               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Bulgogi_2.jpg/220px-Bulgogi_2.jpg" alt="" />
+              </figure>
+            <p>
+              불고기(영어: Bulgogi)는 쇠고기를 양념에 재우고 야채를 넣고 자작하게 만든 대한민국의 전통 고기요리이다. 돼지고기로 만든 것은 따로 돼지 불고기라고 한다. 구이에는 결합 조직이 적고 지방이 조금씩 산재해 있는 고기가 맛이 있고 연하기 때문에 안심이나 등심 등의 부위가 가장 많이 사용된다.
+            </p>
+          </div>
+        </article>
       </div>
     </section>
   </div>
