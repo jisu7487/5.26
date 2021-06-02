f8764b21fa77af1b58f1415f65515b4b59e8112a
jisu7487 <jisu2043@gmail.com>
2021-06-02 09:12:15 +0900

    6.02

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nav class="breadcrumb">
+      <ul>
+        <li><nuxt-link to="/"><i class="fas fa-home"></i> 홈으로</nuxt-link></li>
+      </ul>
+    </nav>
+    <nuxt/>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <i class="fas fa-copyright"></i> 2020-2021, 모든 저작권은 <strong>나지수</strong>에게 있습니다. 
+      </div>
+    </footer>
+  </div>
+</template>
---
+++ update.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "6.02"
+git push -u origin master
