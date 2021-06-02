d8b06d3597b7a1404fbe72a64699ded31e798afe
jisu7487 <jisu2043@gmail.com>
2021-06-02 10:12:19 +0900

    add tableeeee

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
           <i class="fas fa-utensils"></i>음식 정보
         </p>
         <p class="subtitle">
-          한식종류와 칼로리 정보
+          한식종류와 칼로리 정보 dd
         </p>
       </div>
     </section>
@@ -25,6 +25,34 @@
             </figure>
             밥에 각종 나물과 각종 장[2], 그리고 기타 동물성 재료(계란, 고기, 통조림 참치 등)을 넣고 비벼서 만드는 요리다. 장으로 고추장을 가장 많이 사용하지만, 간장을 소스로 사용하는 경우도 있고, 그 쪽이 원래 요리법인 것으로 알려져 있다. 현대에 와서는 매운 것을 잘 먹지 못하는 외국인에게 비빔밥을 만들어 줄 때 간장을 소스로 이용하곤 하는데, 후술할 마이클 잭슨 비빔밥이 한때 유행했으며, 가장 최근에 미디어에 등장한 경우로는 윤식당2에서 간장 비빔밥이 주 메뉴로 등장한 바 있다
           </div>
+        </article>
+          <article class="notification">
+          <table class="table is-striped is-hoverable is-fullwidth">
+            <thead>
+              <tr>
+                <th>분류</th>
+                <th>이름</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>계</td>
+                <td>밥류<small>(Rice)</small></td>
+              </tr>
+              <tr>
+                <td>열량</td>
+                <td>500kcal<small>(Calorie)</small></td>
+              </tr>
+              <tr>
+                <td>조리시간</td>
+                <td>15분<small>(Time)</small></td>
+              </tr>
+              <tr class="is-selected">
+                <td>탄수화물</td>
+                <td>90g<small>(Carbohydrate)</small></td>
+              </tr>
+            </tbody>
+          </table>
         </article>
       </div>
       <div class="column">
--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "6.02"
+git commit -m "add tableeeee"
 git push -u origin master
