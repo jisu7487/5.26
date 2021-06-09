743626791007210474e5f596da68a464d0392055
jisu7487 <jisu2043@gmail.com>
2021-06-09 11:55:31 +0900

    JN test2

--- pages/food.vue
+++ pages/food.vue
@@ -46,7 +46,7 @@
 			</table>
 			<div class="content">
 				<p class="tag is-danger">출처</p>
-				<a href="https://www.daegufood.go.kr/kor/xml/tastyfood.html?f_taste=1">Dog CEO</a><br />
+				<a href="https://data.go.kr/data/15057236/openapi.do">Dog CEO</a><br />
 				<a
 					class="button is-primary is-small"
 					href="https://www.daegufood.go.kr/kor/xml/tastyfood.html?f_taste=1"
