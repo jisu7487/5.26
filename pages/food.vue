62038ea25b664a0768c284be2c85f5960a0cc056
jisu7487 <jisu2043@gmail.com>
2021-06-09 11:41:25 +0900

    food page test2

--- pages/food.vue
+++ pages/food.vue
@@ -46,7 +46,7 @@
 			</table>
 			<div class="content">
 				<p class="tag is-danger">출처</p>
-				<a href="https://dog.ceo/dog-api/documentation/">Dog CEO</a><br />
+				<a href="https://tools.myfooddata.com/nutrition-facts/508628/wt1/1">Dog CEO</a><br />
 				<a
 					class="button is-primary is-small"
 					href="https://dog.ceo/api/breeds/list/all"
