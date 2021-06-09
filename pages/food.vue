308b2ef3bce02f05880d62b935b815716be40937
jisu7487 <jisu2043@gmail.com>
2021-06-09 12:22:47 +0900

    test3  ~~

--- pages/food.vue
+++ pages/food.vue
@@ -47,7 +47,7 @@
 			<div class="content">
 				<a
 					class="button is-primary is-small"
-					href="https://raw.githubusercontent.com/swmaestro/somat/gh-pages/somalife.json"
+					href="https://raw.githubusercontent.com/terrenjpeterson/caloriecounter/master/src/data/foods.json"
 					>List all breeds</a
 				>
 			</div>
@@ -59,7 +59,7 @@
 	import axios from 'axios';
 	export default {
 		async asyncData() {
-			const foodBreeds = await axios.get('https://raw.githubusercontent.com/swmaestro/somat/gh-pages/somalife.json');
+			const foodBreeds = await axios.get('https://raw.githubusercontent.com/terrenjpeterson/caloriecounter/master/src/data/foods.json');
 			//alert(Object.keys(foodBreeds));
 			return {
 				tablefoodBreeds: foodBreeds.data.message,
