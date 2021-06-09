52db5372297e6256f5c3ef2e8069bc0af3683b97
jisu7487 <jisu2043@gmail.com>
2021-06-09 11:09:23 +0900

    test 1

---
+++ pages/food.vue
@@ -0,0 +1,72 @@
+<template>
+	<div>
+		<section class="hero is-warning">
+			<div class="hero-body">
+				<p class="title"><i class="fas fa-dog"></i> 강아지 품종</p>
+				<p class="subtile">
+					다양한 강아지 품종을 함께 공부합시다.
+				</p>
+			</div>
+		</section>
+		<hr />
+		<section class="columns"></section>
+		<div class="column">
+			<table class="table">
+				<thead>
+					<th>번호</th>
+					<th>강아지 품종</th>
+					<th>강아지 아품종</th>
+				</thead>
+				<tbody>
+					<template v-for="pos in tableDogKeys.length">
+						<tr :key="pos">
+							<td>{{ pos }}</td>
+							<td>{{ tableDogKeys[pos - 1] }}</td>
+							<td>
+								<span
+									class="tag is-black"
+									v-if="
+										((ar = tablefood[tableDogKeys[pos - 1]]),
+										ar.length == 0)
+									"
+									>없음</span
+								>
+								<span v-else
+									><template v-for="subbreed in ar"
+										><span class="tag is-success" :key="subbreed">{{
+											subbreed
+										}}</span
+										>&nbsp;</template
+									>
+								</span>
+							</td>
+						</tr>
+					</template>
+				</tbody>
+			</table>
+			<div class="content">
+				<p class="tag is-danger">출처</p>
+				<a href="https://dog.ceo/dog-api/documentation/">Dog CEO</a><br />
+				<a
+					class="button is-primary is-small"
+					href="https://dog.ceo/api/breeds/list/all"
+					>List all breeds</a
+				>
+			</div>
+		</div>
+		<div class="column"></div>
+	</div>
+</template>
+<script>
+	import axios from 'axios';
+	export default {
+		async asyncData() {
+			const food = await axios.get('https://dog.ceo/api/breeds/list/all');
+			//alert(Object.keys(food));
+			return {
+				tablefood: food.data.message,
+				tableDogKeys: Object.keys(food.data.message),
+			};
+		},
+	};
+</script>
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "add food  page "
+git commit -m "test 1 "
 git push -u origin master
