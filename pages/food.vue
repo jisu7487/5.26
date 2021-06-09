f43d895a117d65e158e12dce11fa93d2df741a7e
jisu7487 <jisu2043@gmail.com>
2021-06-09 12:02:11 +0900

    JN test2

--- pages/food.vue
+++ pages/food.vue
@@ -1,72 +1,52 @@
 <template>
-	<div>
-		<section class="hero is-warning">
-			<div class="hero-body">
-				<p class="title"><i class="fas fa-dog"></i> 강아지 품종</p>
-				<p class="subtile">
-					다양한 강아지 품종을 함께 공부합시다.
-				</p>
-			</div>
-		</section>
-		<hr />
-		<section class="columns"></section>
-		<div class="column">
-			<table class="table">
-				<thead>
-					<th>이름</th>
-					<th>몰라</th>
-					<th>태그</th>
-				</thead>
-				<tbody>
-					<template v-for="pos in tableFoodKeys.length">
-						<tr :key="pos">
-							<td>{{ pos }}</td>
-							<td>{{ tableFoodKeys[pos - 1] }}</td>
-							<td>
-								<span
-									class="tag is-black"
-									v-if="
-										((ar = tablefood[tableFoodKeys[pos - 1]]),
-										ar.length == 0)
-									"
-									>없음</span
-								>
-								<span v-else
-									><template v-for="subbreed in ar"
-										><span class="tag is-success" :key="subbreed">{{
-											subbreed
-										}}</span
-										>&nbsp;</template
-									>
-								</span>
-							</td>
-						</tr>
-					</template>
-				</tbody>
-			</table>
-			<div class="content">
-				<p class="tag is-danger">출처</p>
-				<a href="https://data.go.kr/data/15057236/openapi.do">Dog CEO</a><br />
-				<a
-					class="button is-primary is-small"
-					href="https://www.daegufood.go.kr/kor/xml/tastyfood.html?f_taste=1"
-					>List all breeds</a
-				>
-			</div>
-		</div>
-		<div class="column"></div>
-	</div>
+  <div>
+    <section class="hero is-link">
+      <div class="hero-body">
+        <p class="title">
+          <i class="fas fa-glass-martini-alt" /> 지역별 맛집
+        </p>
+        <p class="subtitle">
+          맛집을 추천해 드립니다
+        </p>
+      </div>
+    </section>
+
+    <hr>
+    <section class="columns" />
+    <div class="colums" />
+    <table class="table">
+      <thead>
+        <tr>
+          <th>번호</th>
+          <th>종류</th>
+          <th>맛집</th>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-for="pos in tablefood.length">
+          <tr :key="pos">
+            <td>{{ pos }}</td>
+            <td>{{ tablefood[pos - 1].family }}</td>
+            <td>{{ tablefood[pos - 1].food }}</td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+    <div class="colums" />
+  </div>
 </template>
+
 <script>
-	import axios from 'axios';
-	export default {
-		async asyncData() {
-			const food = await axios.get('https://www.daegufood.go.kr/kor/xml/tastyfood.html?f_taste=1');
-			//alert(Object.keys(food));
-			return {
-				tablefood: food.data.message,
-				tableFoodKeys: Object.keys(food.data.message),
-			};
-		},
-	};
+import axios from 'axios'
+export default {
+  async asyncData () {
+    const foodd = await axios.get('https://www.daegufood.go.kr/kor/xml/tastyfood.html?f_taste=1')
+    alert(Object.keys(foodd.data[0]))
+    return {
+      tablefood: foodd.data,
+      tablefoodkey: Object.keys(foodd.data[0]
+      )
+    }
+  }
+}
 </script>
