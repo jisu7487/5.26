47a4eee8caa4f46f2d978cf24ca1a1565f76b791
jisu7487 <jisu2043@gmail.com>
2021-06-09 12:19:09 +0900

    test3  ~~

--- pages/food.vue
+++ pages/food.vue
@@ -1,51 +1,70 @@
 <template>
-  <div>
-    <section class="hero is-link">
-      <div class="hero-body">
-        <p class="title">
-          <i class="fas fa-glass-martini-alt" /> 맛집 리스트
-        </p>
-        <p class="subtitle">
-         맛집을 평가해드립니다
-        </p>
-      </div>
-    </section>
-
-    <hr>
-    <section class="columns" />
-    <div class="colums" />
-    <table class="table">
-      <thead>
-        <tr>
-          <th>번호</th>
-          <th>이름</th>
-          <th>content</th>
-        </tr>
-      </thead>
-      <tbody>
-        <template v-for="pos in tablefoodKeys.length">
-          <tr :key="pos">
-            <td>{{ pos }}</td>
-            <td>{{ tablefood[pos - 1].food }}</td>
-          </tr>
-        </template>
-      </tbody>
-    </table>
-    <div class="colums" />
-  </div>
+	<div>
+		<section class="hero is-warning">
+			<div class="hero-body">
+				<p class="title"><i class="fas fa-dog"></i> 강아지 품종</p>
+				<p class="subtile">
+					다양한 강아지 품종을 함께 공부합시다.
+				</p>
+			</div>
+		</section>
+		<hr />
+		<section class="columns"></section>
+		<div class="column">
+			<table class="table">
+				<thead>
+					<th>번호</th>
+					<th>강아지 품종</th>
+					<th>강아지 아품종</th>
+				</thead>
+				<tbody>
+					<template v-for="pos in tablefoodKeys.length">
+						<tr :key="pos">
+							<td>{{ pos }}</td>
+							<td>{{ tablefoodKeys[pos - 1] }}</td>
+							<td>
+								<span
+									class="tag is-black"
+									v-if="
+										((ar = tablefoodBreeds[tablefoodKeys[pos - 1]]),
+										ar.length == 0)
+									"
+									>없음</span
+								>
+								<span v-else
+									><template v-for="subbreed in ar"
+										><span class="tag is-success" :key="subbreed">{{
+											subbreed
+										}}</span
+										>&nbsp;</template
+									>
+								</span>
+							</td>
+						</tr>
+					</template>
+				</tbody>
+			</table>
+			<div class="content">
+				<a
+					class="button is-primary is-small"
+					href="https://dog.ceo/api/breeds/list/all"
+					>List all breeds</a
+				>
+			</div>
+		</div>
+		<div class="column"></div>
+	</div>
 </template>
-
 <script>
-import axios from 'axios'
-export default {
-  async asyncData () {
-    const foodlist = await axios.get('https://raw.githubusercontent.com/swmaestro/somat/gh-pages/somalife.json')
-    alert(Object.keys(foodlist.data[0]))
-    return {
-      tablefood: foodlist.data,
-      tablefoodkey: Object.keys(foodlist.data[0]
-      )
-    }
-  }
-}
+	import axios from 'axios';
+	export default {
+		async asyncData() {
+			const foodBreeds = await axios.get('https://dog.ceo/api/breeds/list/all');
+			//alert(Object.keys(foodBreeds));
+			return {
+				tablefoodBreeds: foodBreeds.data.message,
+				tablefoodKeys: Object.keys(foodBreeds.data.message),
+			};
+		},
+	};
 </script>
